4f1b209ae5ac3ffdf90ab10f558d049ef21e60f3
dinesh7-tech <dinxsh7@gmail.com>
2026-08-07 08:59:41 +0530

    Completely remove functionMiddleware and attachSupabaseAuth reference from start.ts to avoid runtime createMiddleware error

--- src/start.ts
+++ src/start.ts
@@ -1,10 +1,10 @@
 import { createStart } from "@tanstack/react-start";
-import { attachSupabaseAuth } from "@/integrations/supabase/auth-attacher";
 
 export const startInstance = createStart(() => ({
-  functionMiddleware: [attachSupabaseAuth],
+  functionMiddleware: [],
   requestMiddleware: [],
 }));
+
 
 
 
